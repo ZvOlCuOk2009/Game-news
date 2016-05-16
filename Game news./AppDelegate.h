1bf540fbb3659830eeb656146fddefb4995c66d8
Zvigun Alexandr <mircamnia@ukr.net>
2016-05-16 21:54:21 +0300

    Initial Commit

---
+++ Game news./AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Game news.
+//
+//  Created by Mac on 16.05.16.
+//  Copyright © 2016 Tsvigun Alexandr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Game news./ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Game news.
+//
+//  Created by Mac on 16.05.16.
+//  Copyright © 2016 Tsvigun Alexandr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
